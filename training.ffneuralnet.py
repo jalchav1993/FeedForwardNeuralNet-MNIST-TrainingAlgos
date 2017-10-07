afece38ce3fd990c54c1ab57588be6220b5e228e
Aex <aex@Aexs-MacBook-Pro.local>
2017-10-06 22:24:26 -0600

    pseudo inverse with gradient descent learning for convolutional neural netowrk

--- training.ffneuralnet.py
+++ training.ffneuralnet.py
@@ -1,6 +1,7 @@
 
 import sys
 from scipy.special import expit
+from matplotlib import pyplot as plt
 import numpy as np
 sys.path.insert(0,'./')
 from ffneuralnet import *
@@ -10,25 +11,21 @@ BATCH_SIZE = 777;
 BSPEC = 0.1999;
 WSPEC = 0.1999;
 DELTASPEC = 0.0666;
-STEP = 1313;
-H0_LEN = 900;
-H1_LEN = 600;
-L = 0.0066;
-def findWeightsRandom():
-  w = [];
-  b = [];
-  #let Wbest=W0,W1,W2,b0,b1,b2 be a randomly-chosen set of weights
-  w.append(findRandomWeight(784, H0_LEN, WSPEC));
-  w.append(findRandomWeight(H0_LEN, H1_LEN, WSPEC)); 
-  w.append(findRandomWeight(H1_LEN, 10, WSPEC));
-  b.append(findRandomBias(H0_LEN, BSPEC));
-  b.append(findRandomBias(H1_LEN, BSPEC));
-  b.append(findRandomBias(10, BSPEC));
+EPOCH = 99;
+STEP = 66;
+H0_LEN = 60;
+H1_LEN = 30;
+L = 0.000969;
+def randomSet():
+  return ();
+def findWeightsRandom(w,b):
+  m_error = [];
   (batchx, batchy) = findRandomBatch(F.x_train, F.y_train);
   e_current = get_error(w, b, batchx,batchy);
-  for i in range(STEP):
+  m_error.append(e_current);
+  for i in range(STEP*10 + 6):
 #     # let xb, yb be a randomly chosen subset of (X, Y)
-    (deltaw, deltab) = getDelta(w, b);
+    (deltaw, deltab) = getRandomSet(DELTASPEC);
     (wnew, bnew) = add(w, b, deltaw, deltab);
     e_trial = get_error(wnew, bnew, batchx,batchy);
     (batchx, batchy) = findRandomBatch(F.x_train, F.y_train);
@@ -37,7 +34,8 @@ def findWeightsRandom():
       w = wnew;
       b = bnew;
       e_current = e_trial;
-  return (w,b);
+      m_error.append(e_current);
+  return (w,b,m_error);
 def findWeightsPseudoinverse():
   w = [];
   b = [];
@@ -54,40 +52,40 @@ def findWeightsPseudoinverse():
   b.append(np.mean(y_l));
   w.append(np.dot(np.linalg.pinv(h1), (y_l - b[2])));
   return (w,b)
-def findWeightsBackDrop():
-  w = [];
-  b = [];
-  #let Wbest=W0,W1,W2,b0,b1,b2 be a randomly-chosen set of weights
-  w.append(findRandomWeight(784, H0_LEN, WSPEC));
-  w.append(findRandomWeight(H0_LEN, H1_LEN, WSPEC)); 
-  w.append(findRandomWeight(H1_LEN, 10, WSPEC));
-  b.append(findRandomBias(H0_LEN, BSPEC));
-  b.append(findRandomBias(H1_LEN, BSPEC));
-  b.append(findRandomBias(10, BSPEC));
-  #magick
-  for i in range(100):
-    print i;
+def findWeightsBackDrop(w, b):
+  m_error = [];
+  for i in range(EPOCH):
+    #this is magick
+    new_error_sum = 0;
     (batchx, batchy) = findRandomBatch(F.x_train, F.y_train);
-    y_l = logit([y * 0.9 + 0.05 for y in batchy]);
-    h0 = F.relu(np.dot(batchx,w[0])+ b[0]);
-    h1 = F.relu(np.dot(h0,w[1]) + b[1]);
-    P = F.sigmoid(np.dot(h1,w[2]) + b[2]);
-    #dP = np.multiply(np.multiply((P - batchy), P), (1-P));
-    dP_ = (P - y_l)* P;
-    dP= dP_*  (1-P);
-    #transposed weights concatenated
-    W_t = [np.transpose(weight) for weight in w];
-    dH1 = np.dot(dP,W_t[2]) * sign(h1);
-    dH0 = np.dot(dH1,W_t[1])* sign(h0);
-    #what is the significance of this transformation? I wonder
-    w[2] = w[2] - L * np.dot(np.transpose(h1), dP);
-    w[1] = w[1] - L * np.dot(np.transpose(h0), dH1);
-    w[0] = w[0] - L * np.dot(np.transpose(batchx), dH0);
-    b[2] = b[2] - L * np.sum(dP, axis = 0);
-    b[1] = b[1] - L * np.sum(dH1, axis = 0);
-    b[0] = b[0] - L * np.sum(dH0, axis = 0);
-    print (get_mean_error(y_l,P));
-  return (w,b);
+    y_l = [y * 0.9 + 0.05 for y in batchy];
+    for j in range(STEP):
+      h0 = F.relu(np.dot(batchx,w[0])+ b[0]);
+      h1 = F.relu(np.dot(h0,w[1]) + b[1]);
+      P = F.sigmoid(np.dot(h1,w[2]) + b[2]);
+      #is this the hadamard product
+      dP_ = np.multiply((P - y_l),P);
+      dP= np.multiply(dP_ ,(1-P));
+      #transposed weights concatenated
+      W_t = [np.transpose(weight) for weight in w];
+      dH1 = np.multiply(np.dot(dP,W_t[2]), sign(h1));
+      dH0 = np.multiply(np.dot(dH1,W_t[1]),sign(h0));
+      new_error = get_mean_error(batchy,P);
+      new_error_sum += new_error;
+      w[0] = w[0] - L * np.dot(np.transpose(batchx), dH0);
+      w[1] = w[1] - L * np.dot(np.transpose(h0), dH1);
+      w[2] = w[2] - L * np.dot(np.transpose(h1), dP);
+      b[0] = b[0] - L * np.sum(dH0, axis = 0);
+      b[1] = b[1] - L * np.sum(dH1, axis = 0);
+      b[2] = b[2] - L * np.sum(dP, axis = 0);
+      #print new_error;
+      #print (i, j);
+    new_error_sum/=STEP;
+    m_error.append(new_error_sum);
+    if new_error_sum < 0.00444:
+      break;
+    print ("Epoch: %d, error %f" %(i, new_error_sum));
+  return (w,b, m_error);
 def logit(y):
   return [np.log(label) - np.log(1-label) for label in y];
   
@@ -107,16 +105,17 @@ def findRandomWeight(row, col, spec):
 def findRandomBias(size, spec):
   return (np.random.rand(size)-0.5)*spec;
 
-def getDelta(w, b):
-  return ([findRandomWeight(784, H0_LEN, DELTASPEC),
-    findRandomWeight(H0_LEN, H1_LEN, DELTASPEC),
-    findRandomWeight(H1_LEN, 10, DELTASPEC)],
-    [findRandomBias(H0_LEN, DELTASPEC),
-    findRandomBias(H1_LEN, DELTASPEC),
-    findRandomBias(10, DELTASPEC)]);
-    
+def getRandomSet(spec):
+  return ([findRandomWeight(784, H0_LEN, spec),
+    findRandomWeight(H0_LEN, H1_LEN, spec),
+    findRandomWeight(H1_LEN, 10, spec)],
+    [findRandomBias(H0_LEN, spec),
+    findRandomBias(H1_LEN, spec),
+    findRandomBias(10, spec)]);
+def getlambda(n):
+  return [0.00066 for i in range(n)];
 def get_mean_error(y,p):
-  return np.square(np.mean(y-p));
+  return np.mean(np.square(y-p));
     
 def get_error(w, b, x, y):
   #need help understanding why this
@@ -133,14 +132,28 @@ def add(w,b, changew, changeb):
 def findRandomBatch(x, y):
   indexes = np.random.randint(0,TRAINING_SIZE,BATCH_SIZE);
   return ([x[i] for i in indexes], [y[i] for i in indexes]);
-
-#(w, b) = findWeightsPseudoinverse();
-(w, b) = findWeightsBackDrop();
-out = F.feedForward(F.x_test, w, b);
-np.set_printoptions(threshold=np.inf) #Print complete arrays
-acc, pred, loss = F.eval_out(out, F.test_class,F.y_test);
-print("Accuracy =",acc);
-print("Loss =",loss);
-print("Confusion_matrix:");
-print(F.confusion_matrix(F.test_class,pred));
-F.display_errors(F.x_test,F.test_class,pred,5);
+  
+def printTest(out, m_error):
+  np.set_printoptions(threshold=np.inf) #Print complete arrays
+  acc, pred, loss = F.eval_out(out, F.test_class,F.y_test);
+  print("Accuracy =",acc);
+  print("Loss =",loss);
+  print("Confusion_matrix:");
+  print(F.confusion_matrix(F.test_class,pred));
+  F.display_errors(F.x_test,F.test_class,pred,5);
+  plt.plot(m_error);
+  plt.show();
+#(w_init, b_init) = getRandomSet(WSPEC);
+#algorithm 1
+#(w_rand, b_rand, m_error_rand) = findWeightsRandom(w_init,b_init);
+# #algo 2
+(w_psinv, b_psinv) = findWeightsPseudoinverse();
+# #algo 3
+#(w_bd, b_bd, m_error_bd) = findWeightsBackDrop(w_init,b_init);
+# #algo 4
+(w_bd_inv, b_bd_inv, m_error_inv) = findWeightsBackDrop(w_psinv,b_psinv);
+# #printing
+#printTest(F.feedForward(F.x_test, w_rand,b_rand), m_error_rand);
+#printTest(F.feedForward(F.x_test, w_psinv,b_psinv),'');
+#printTest(F.feedForward(F.x_test, w_bd,b_bd),m_error_bd);
+printTest(F.feedForward(F.x_test, w_bd_inv,b_bd_inv),m_error_inv);
