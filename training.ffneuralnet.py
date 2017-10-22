61bd879d2ce975a49f3b999d2432b7b65899f111
Aex <aex@Aexs-MacBook-Pro.local>
2017-10-22 06:04:54 -0600

    90% accuracy, still looking for the bug that does not let it go forward

--- training.ffneuralnet.py
+++ training.ffneuralnet.py
@@ -1,34 +1,39 @@
-
+#Author Jesus Chavez
+#trains for a neural network
+#uses test set augmentation
+#fixed the naming issue, Python doesNotUseThisStyle
 import sys
+from math import sqrt 
 from scipy.special import expit
 from matplotlib import pyplot as plt
 import numpy as np
 sys.path.insert(0,'./')
 from ffneuralnet import *
+from augment import *
 F = api();
 TRAINING_SIZE = len(F.x_train);
-BATCH_SIZE = 777;
+BATCH_SIZE = 77;
 BSPEC = 0.1999;
 WSPEC = 0.1999;
 DELTASPEC = 0.0666;
 EPOCH = 99;
 STEP = 66;
+X0_LEN = 784;
 H0_LEN = 60;
 H1_LEN = 30;
-L = 0.000969;
-def randomSet():
-  return ();
-def findWeightsRandom(w,b):
+L = 0.0006969;
+
+def find_weights_random(w,b):
   m_error = [];
-  (batchx, batchy) = findRandomBatch(F.x_train, F.y_train);
+  (batchx, batchy) = find_random_batch(F.x_train, F.y_train);
   e_current = get_error(w, b, batchx,batchy);
   m_error.append(e_current);
   for i in range(STEP*10 + 6):
 #     # let xb, yb be a randomly chosen subset of (X, Y)
-    (deltaw, deltab) = getRandomSet(DELTASPEC);
+    (deltaw, deltab) = get_random_set(DELTASPEC);
     (wnew, bnew) = add(w, b, deltaw, deltab);
     e_trial = get_error(wnew, bnew, batchx,batchy);
-    (batchx, batchy) = findRandomBatch(F.x_train, F.y_train);
+    (batchx, batchy) = find_random_batch(F.x_train, F.y_train);
     print ("iteration: %d e0:%f e1:%f" %(i, e_current,e_trial));
     if e_trial < e_current: 
       w = wnew;
@@ -36,15 +41,17 @@ def findWeightsRandom(w,b):
       e_current = e_trial;
       m_error.append(e_current);
   return (w,b,m_error);
-def findWeightsPseudoinverse():
+  
+def find_weights_pseudoinverse():
   w = [];
   b = [];
   #let Wbest=W0,W1,W2,b0,b1,b2 be a randomly-chosen set of weights
-  w.append(findRandomWeight(784, H0_LEN, WSPEC));
-  w.append(findRandomWeight(H0_LEN, H1_LEN, WSPEC)); 
-  b.append(findRandomBias(H0_LEN, BSPEC));
-  b.append(findRandomBias(H1_LEN, BSPEC));
-  (batchx, batchy) = findRandomBatch(F.x_train, F.y_train);
+  (batchx, batchy) = find_random_batch(F.x_train, F.y_train);
+  print X0_LEN;
+  w.append(find_random_weight(X0_LEN, H0_LEN, WSPEC));
+  w.append(find_random_weight(H0_LEN, H1_LEN, WSPEC)); 
+  b.append(find_random_bias(H0_LEN, BSPEC));
+  b.append(find_random_bias(H1_LEN, BSPEC));
   h0 = F.relu(np.dot(batchx,w[0])+ b[0]);
   h1 = F.relu(np.dot(h0,w[1]) + b[1]);
   y_l = [y * 0.9 + 0.05 for y in batchy];
@@ -52,12 +59,15 @@ def findWeightsPseudoinverse():
   b.append(np.mean(y_l));
   w.append(np.dot(np.linalg.pinv(h1), (y_l - b[2])));
   return (w,b)
-def findWeightsBackDrop(w, b):
+  
+def find_weights_backdrop(w, b):
   m_error = [];
   for i in range(EPOCH):
-    #this is magick
+    # this is magick
     new_error_sum = 0;
-    (batchx, batchy) = findRandomBatch(F.x_train, F.y_train);
+    (batchx, batchy) = find_random_batch(F.x_train, F.y_train);
+    # augmenting the set
+    # check this with doctor olac
     y_l = [y * 0.9 + 0.05 for y in batchy];
     for j in range(STEP):
       h0 = F.relu(np.dot(batchx,w[0])+ b[0]);
@@ -78,14 +88,13 @@ def findWeightsBackDrop(w, b):
       b[0] = b[0] - L * np.sum(dH0, axis = 0);
       b[1] = b[1] - L * np.sum(dH1, axis = 0);
       b[2] = b[2] - L * np.sum(dP, axis = 0);
-      #print new_error;
-      #print (i, j);
     new_error_sum/=STEP;
     m_error.append(new_error_sum);
-    if new_error_sum < 0.00444:
+    if committee(new_error_sum):
       break;
     print ("Epoch: %d, error %f" %(i, new_error_sum));
   return (w,b, m_error);
+  
 def logit(y):
   return [np.log(label) - np.log(1-label) for label in y];
   
@@ -93,27 +102,28 @@ def sign(y):
   for x in np.nditer(y, op_flags=['readwrite']):
       x[...] = getdif(x);
   return y;
-  
+def committee(error):
+  #tests by comitte
+  return error < 0.002013013;
 def getdif(x):
   if x > 0: return 1 
   elif x == 0: return 0
   else: return -1;
   
-def findRandomWeight(row, col, spec):
+def find_random_weight(row, col, spec):
   return (np.random.rand(row,col)-0.5)*spec;
   
-def findRandomBias(size, spec):
+def find_random_bias(size, spec):
   return (np.random.rand(size)-0.5)*spec;
 
-def getRandomSet(spec):
-  return ([findRandomWeight(784, H0_LEN, spec),
-    findRandomWeight(H0_LEN, H1_LEN, spec),
-    findRandomWeight(H1_LEN, 10, spec)],
-    [findRandomBias(H0_LEN, spec),
-    findRandomBias(H1_LEN, spec),
-    findRandomBias(10, spec)]);
-def getlambda(n):
-  return [0.00066 for i in range(n)];
+def get_random_set(spec):
+  return ([find_random_weight(X0_LEN, H0_LEN, spec),
+    find_random_weight(H0_LEN, H1_LEN, spec),
+    find_random_weight(H1_LEN, 10, spec)],
+    [find_random_bias(H0_LEN, spec),
+    find_random_bias(H1_LEN, spec),
+    find_random_bias(10, spec)]);
+
 def get_mean_error(y,p):
   return np.mean(np.square(y-p));
     
@@ -129,11 +139,18 @@ def add(w,b, changew, changeb):
     b[1]+changeb[1], 
     b[2]+changeb[2]]);
     
-def findRandomBatch(x, y):
+def tset_augmentation(X, Y):
+  #model in ./augment.py module
+  model = augment_model(1,X,Y,1); 
+  return model.build();
+
+def find_random_batch(x, y):
   indexes = np.random.randint(0,TRAINING_SIZE,BATCH_SIZE);
-  return ([x[i] for i in indexes], [y[i] for i in indexes]);
-  
-def printTest(out, m_error):
+  return augment([x[i] for i in indexes], [y[i] for i in indexes]);
+def augment(x,y):
+  #addhoc 
+  return tset_augmentation(x,y);
+def print_test(out, m_error):
   np.set_printoptions(threshold=np.inf) #Print complete arrays
   acc, pred, loss = F.eval_out(out, F.test_class,F.y_test);
   print("Accuracy =",acc);
@@ -143,17 +160,17 @@ def printTest(out, m_error):
   F.display_errors(F.x_test,F.test_class,pred,5);
   plt.plot(m_error);
   plt.show();
-#(w_init, b_init) = getRandomSet(WSPEC);
+#(w_init, b_init) = get_random_set(WSPEC);
 #algorithm 1
-#(w_rand, b_rand, m_error_rand) = findWeightsRandom(w_init,b_init);
+#(w_rand, b_rand, m_error_rand) = find_weights_random(w_init,b_init);
 # #algo 2
-(w_psinv, b_psinv) = findWeightsPseudoinverse();
+(w_psinv, b_psinv) = find_weights_pseudoinverse();
 # #algo 3
-#(w_bd, b_bd, m_error_bd) = findWeightsBackDrop(w_init,b_init);
+#(w_bd, b_bd, m_error_bd) = find_weights_backdrop(w_init,b_init);
 # #algo 4
-(w_bd_inv, b_bd_inv, m_error_inv) = findWeightsBackDrop(w_psinv,b_psinv);
+(w_bd_inv, b_bd_inv, m_error_inv) = find_weights_backdrop(w_psinv,b_psinv);
 # #printing
-#printTest(F.feedForward(F.x_test, w_rand,b_rand), m_error_rand);
-#printTest(F.feedForward(F.x_test, w_psinv,b_psinv),'');
-#printTest(F.feedForward(F.x_test, w_bd,b_bd),m_error_bd);
-printTest(F.feedForward(F.x_test, w_bd_inv,b_bd_inv),m_error_inv);
+#print_test(F.feedForward(F.x_test, w_rand,b_rand), m_error_rand);
+#print_test(F.feedForward(F.x_test, w_psinv,b_psinv),'');
+#print_test(F.feedForward(F.x_test, w_bd,b_bd),m_error_bd
+print_test(F.feedForward(F.x_test, w_bd_inv,b_bd_inv),m_error_inv);
