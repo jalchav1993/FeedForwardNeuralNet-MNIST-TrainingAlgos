fe3a99541efa6ecf2823df7f94f4530b5380ae62
Aex <aex@Aexs-MacBook-Pro.local>
2017-10-26 03:06:25 -0600

    optimization L2 regularization

--- training.ffneuralnet.py
+++ training.ffneuralnet.py
@@ -10,26 +10,32 @@ import numpy as np
 sys.path.insert(0,'./')
 from ffneuralnet import *
 from augment import *
+from dropout import *
+from random import sample
+
 F = api();
-TRAINING_SIZE = len(F.x_train);
 BATCH_SIZE = 77;
 BSPEC = 0.1999;
 WSPEC = 0.1999;
 DELTASPEC = 0.0666;
-EPOCH = 30;
-STEP = 66;
+EPOCH = 14;
+STEP = 46;
 X0_LEN = 784;
-H0_LEN = 60;
-H1_LEN = 30;
-L = 0.00066969;
-
+H0_LEN = 90;
+H1_LEN = 60;
+L = 0.000666;
+dropout_train = dropout_model(F.x_train,F.y_train, F.train_class);
+dropout_test = dropout_model(F.x_test,F.y_test, F.test_class);
+(X,Y, TRAIN_CLASS) = dropout_train.build();
+(X_TEST,Y_TEST, TEST_CLASS) = dropout_test.build();
+TRAINING_SIZE = len(X);
 def find_weights_random(w,b):
   m_error = [];
   (batchx, batchy) = find_random_batch(F.x_train, F.y_train);
   e_current = get_error(w, b, batchx,batchy);
   m_error.append(e_current);
   for i in range(STEP*10 + 6):
-#     # let xb, yb be a randomly chosen subset of (X, Y)
+    # let xb, yb be a randomly chosen subset of (X, Y)
     (deltaw, deltab) = get_random_set(DELTASPEC);
     (wnew, bnew) = add(w, b, deltaw, deltab);
     e_trial = get_error(wnew, bnew, batchx,batchy);
@@ -46,8 +52,7 @@ def find_weights_pseudoinverse():
   w = [];
   b = [];
   #let Wbest=W0,W1,W2,b0,b1,b2 be a randomly-chosen set of weights
-  (batchx, batchy) = find_random_batch(F.x_train, F.y_train);
-  print X0_LEN;
+  (batchx, batchy) = find_random_batch(X, Y);
   w.append(find_random_weight(X0_LEN, H0_LEN, WSPEC));
   w.append(find_random_weight(H0_LEN, H1_LEN, WSPEC)); 
   b.append(find_random_bias(H0_LEN, BSPEC));
@@ -66,7 +71,7 @@ def find_weights_backdrop(w, b):
   for i in range(EPOCH):
     # this is magick
     new_error_sum = 0;
-    (batchx, batchy) = find_random_batch(F.x_train, F.y_train);
+    (batchx, batchy) = find_random_batch(X, Y);
     # augmenting the set
     # check this with doctor olac
     y_l = [y * 0.9 + 0.05 for y in batchy];
@@ -95,16 +100,22 @@ def find_weights_backdrop(w, b):
       break;
     print ("Epoch: %d, error %f" %(i, new_error_sum));
   return (w,b, m_error);
-def find_weights_backdrop_l2(w, b, alpha_reg, beta_reg):
-  m_error = [];
+
+def find_weights_backdrop_l2(train_v, test_v,w, b, alpha_reg, beta, flag):
+  (x_train,y_train, train_class) = train_v
+  (x_test,y_test, test_class) = test_v
+  acc_test =[];
+  acc_train = [];
   l = L;
   alpha_it = 0;
+  gW0 = gW1 = gW2 = gb0 = gb1 = gb2 =0;
   for i in range(EPOCH):
     # this is magick
-    new_error_sum = 0;
-    (batchx, batchy) = find_random_batch(F.x_train, F.y_train);
+    acc_sum_train = 0;
+    acc_sum_test = 0;
+    test_set = test_class;
+    (batchx, batchy) = find_random_batch(x_train, y_train);
     # augmenting the set
-    # check this with doctor olac
     y_l = [y * 0.9 + 0.05 for y in batchy];
     for j in range(STEP):
       h0 = F.relu(np.dot(batchx,w[0])+ b[0]);
@@ -117,38 +128,53 @@ def find_weights_backdrop_l2(w, b, alpha_reg, beta_reg):
       W_t = [np.transpose(weight) for weight in w];
       dH1 = np.multiply(np.dot(dP,W_t[2]), sign(h1));
       dH0 = np.multiply(np.dot(dH1,W_t[1]),sign(h0));
-      new_error = get_mean_error(batchy,P);
-      new_error_sum += new_error;
-      w[0] = w[0] - l * np.dot(np.transpose(batchx), dH0);
-      w[1] = w[1] - l * np.dot(np.transpose(h0), dH1);
-      w[2] = w[2] - l * np.dot(np.transpose(h1), dP);
-      b[0] = b[0] - l * np.sum(dH0, axis = 0);
-      b[1] = b[1] - l * np.sum(dH1, axis = 0);
-      b[2] = b[2] - l * np.sum(dP, axis = 0);
-    new_error_sum/=STEP;
-    m_error.append(new_error_sum);
-    if committee(new_error_sum):
-      break;
-    print ("Epoch: %d, error %f" %(i, new_error_sum));
-  return (w,b, m_error);
-def update_lambda(l, new_error, old_error):
-  #check this
-  if (new_error > old_error):
-    operator = -1;
-  else:
-    operator = 1;
-  error_convergence = np.absolute(new_error - old_error);
-  print "error %f"% error_convergence;
-  if(error_convergence>= 0.15): 
-    return l + operator*0.000000701;
-  elif(error_convergence>= 0.04 and error_convergence < 0.15):
-    return l + operator*0.00001050;
-  elif(error_convergence>= 0.01 and error_convergence < 0.04):
-    return l + operator*0.00001100;
-  elif(error_convergence>= 0.002 and error_convergence < 0.01): 
-    return l + operator*0.00002125;
-  else:
-    return l + 0.0004150;
+      batch_x_t =np.transpose(batchx);
+      h0_t = np.transpose(h0);
+      h1_t = np.transpose(h1);
+      #update weights and biases
+      gW2 = alpha_it * gW2 + (1 - alpha_it) * np.dot(h1_t, dP);
+      gW1 = alpha_it * gW1 + (1 - alpha_it) * np.dot(h0_t, dH1);
+      gW0 = alpha_it * gW0 + (1 - alpha_it) * np.dot(batch_x_t, dH0);
+      
+      gb2 = alpha_it * gb2 + (1 - alpha_it) * np.sum(dP);
+      gb1 = alpha_it * gb1 + (1 - alpha_it) * np.sum(dH1);
+      gb0 = alpha_it * gb0 + (1 - alpha_it) * np.sum(dH0);
+      #new_error = get_mean_error(batchy,P);
+      w[2] = w[2] - l * (gW2 + beta * w[2]);
+      w[1] = w[1] - l * (gW1 + beta * w[1]);
+      w[0] = w[0] - l * (gW0 + beta * w[0]);
+      b[2] = b[2] - l * (gb2 + beta * b[2]);
+      b[1] = b[1] - l * (gb1 + beta * b[1]);
+      b[0] = b[0] - l * (gb0 + beta * b[0]);
+      acc_train_i = get_acc(w, b, x_train, y_train, train_class);
+      acc_test_i = get_acc(w, b, x_test, y_test, test_class);
+      acc_sum_train += acc_train_i;
+      acc_sum_test += acc_test_i;
+    acc_sum_train/=STEP;
+    acc_sum_test/=STEP;
+    acc_train.append(acc_sum_train);
+    acc_test.append(acc_sum_test);
+    alpha_it = alpha_reg;
+    #greedy  
+    print ("Epoch: %d, accuracy test %f, accuracy train %f" %(i, acc_sum_test,acc_sum_train));
+    if (acc_sum_test < 0.39 and flag):
+      print "Greedy: Dropping try when accuracy < 0.39"
+      return run();
+    elif(acc_sum_test >= 0.39 and flag):
+      flag = False;  
+  return (w,b, acc_train, acc_test);
+def run():
+  dropout_train = dropout_model(F.x_train,F.y_train, F.train_class);
+  dropout_test = dropout_model(F.x_test,F.y_test, F.test_class);
+  train_v = (x_train,y_train, train_class) = dropout_train.build();
+  test_v = (x_test,y_test, test_class) = dropout_test.build();
+  training_size = len(x_train);
+  flag = True;
+  alpha_reg = 0.000666;
+  beta = 0.000666;
+  (w_inv, b_inv) = find_weights_pseudoinverse();
+  (w,b, acc_train, acc_test) = find_weights_backdrop_l2(train_v, test_v, w_inv, b_inv, alpha_reg, beta, flag)
+  return (w,b, acc_train, acc_test);
 def logit(y):
   return [np.log(label) - np.log(1-label) for label in y];
   
@@ -178,11 +204,16 @@ def get_random_set(spec):
     find_random_bias(H1_LEN, spec),
     find_random_bias(10, spec)]);
 
+def get_acc(w, b, x ,y, test_set):
+  out = F.feedForward(x, w,b);
+  acc, pred, loss = F.eval_out(out, test_set,y);
+  return acc;
 def get_mean_error(y,p):
   return np.mean(np.square(y-p));
     
 def get_error(w, b, x, y):
   #need help understanding why this
+  
   return np.mean(np.square(y-F.feedForward(x,w, b)));
   
 def add(w,b, changew, changeb):
@@ -193,38 +224,31 @@ def add(w,b, changew, changeb):
     b[1]+changeb[1], 
     b[2]+changeb[2]]);
     
-def tset_augmentation(X, Y):
+def tset_augmentation(x, y, p):
   #model in ./augment.py module
-  model = augment_model(1,X,Y,1); 
+  model = augment_model(p,x,y,p);
   return model.build();
-
+def dropout(x,y):
+  model =dropout_model(x,y);
+  return model.build()
 def find_random_batch(x, y):
-  indexes = np.random.randint(0,TRAINING_SIZE,BATCH_SIZE);
-  return augment([x[i] for i in indexes], [y[i] for i in indexes]);
-def augment(x,y):
-  #addhoc 
-  return tset_augmentation(x,y);
-def print_test(out, m_error):
+  indexes = sample(range(TRAINING_SIZE), BATCH_SIZE)
+  return addhoc([x[i] for i in indexes], [y[i] for i in indexes]);
+def addhoc(x,y):
+  (x,y) = dropout(x,y); 
+  return tset_augmentation(x,y,1);
+def print_test(out, x, y,test_set,acc_set):
   np.set_printoptions(threshold=np.inf) #Print complete arrays
-  acc, pred, loss = F.eval_out(out, F.test_class,F.y_test);
+  acc, pred, loss = F.eval_out(out, test_set,y);
   print("Accuracy =",acc);
   print("Loss =",loss);
   print("Confusion_matrix:");
-  print(F.confusion_matrix(F.test_class,pred));
-  F.display_errors(F.x_test,F.test_class,pred,5);
-  plt.plot(m_error);
+  print(F.confusion_matrix(test_set,pred));
+  F.display_errors(x,test_set,pred,5);
+  plt.plot(acc_set);
   plt.show();
-#(w_init, b_init) = get_random_set(WSPEC);
-#algorithm 1
-#(w_rand, b_rand, m_error_rand) = find_weights_random(w_init,b_init);
-# #algo 2
-(w_psinv, b_psinv) = find_weights_pseudoinverse();
-# #algo 3
-#(w_bd, b_bd, m_error_bd) = find_weights_backdrop(w_init,b_init);
-# #algo 4
-(w_bd_inv, b_bd_inv, m_error_inv) = find_weights_backdrop(w_psinv,b_psinv);
-# #printing
-#print_test(F.feedForward(F.x_test, w_rand,b_rand), m_error_rand);
-#print_test(F.feedForward(F.x_test, w_psinv,b_psinv),'');
-#print_test(F.feedForward(F.x_test, w_bd,b_bd),m_error_bd
-print_test(F.feedForward(F.x_test, w_bd_inv,b_bd_inv),m_error_inv);
+(w,b, acc_train, acc_test) = run();
+te_set_test = F.feedForward(X_TEST, w,b);
+tr_set_test = F.feedForward(X, w,b);
+print_test(te_set_test, X_TEST, Y_TEST, TEST_CLASS, acc_test);
+print_test(tr_set_test, X, Y,TRAIN_CLASS, acc_train);
